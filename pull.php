75267865f74b51cb5596738cef3c92575ec633f7
Timur O <19150752+Timur-O@users.noreply.github.com>
2022-11-06 19:32:57 +0100

    Add deploy file (pull)

---
+++ pull.php
@@ -0,0 +1,23 @@
+<?php
+
+function verify_signature($body, $secret) {
+    $headers = getallheaders();
+    return hash_equals('sha256='.hash_hmac('sha256', $body, $secret), $headers['X-Hub-Signature-256']);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    /**
+     * Declare variables from config.php
+     *
+     * @var $github_secret_webhook string The secret to authenticate the webhook
+     */
+    include_once('config.php');
+
+    $body = file_get_contents('php://input');
+
+    if (verify_signature($body, $github_secret_webhook)) {
+        if (json_decode($body, true)['ref'] == 'refs/heads/main') {
+            shell_exec("git pull");
+        }
+    }
+}
